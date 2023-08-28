c570208aa2992688c70f849ed86e4717e7792952
Sagawala <johnsagawala360@gmail.com>
2023-08-28 00:43:10 -0700

    FLOW

---
+++ Whloop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public Class Whloop{
+    public  static void main(Strings []  args){
+        Scanner vote = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = vote.nextInt();
+        if (age <18){
+            System.out.println("Your not allowed to vote!");
+        }
+
+        else{
+
+            System.out.println("Your allowed to vote");
+        }
+
+    }
+}
